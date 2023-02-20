c961b5cde21989d0b4e6b2c928b76714a67dc352
Sebastian Ruiz <sebasruiz97@gmail.com>
2023-02-20 12:05:25 -0500

    Finalizar aplicacion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  code: any
 
-  constructor() { }
+  constructor(private barcodeScanner: BarcodeScanner, public platform: Platform) { 
+    if (this.platform.is('cordova')) {
+      
+      console.log('I am on a web browser')
+    } else {
+     console.log('im not on a web browser')
+    }
+  }
 
   ngOnInit() {
+    
+  }
+
+  scan(){
+    this.barcodeScanner.scan().then(barcodeData => {
+      this.code = barcodeData.text
+      console.log('Barcode data', this.code);
+     }).catch(err => {
+         console.log('Error', err);
+     });
   }
 
 }
